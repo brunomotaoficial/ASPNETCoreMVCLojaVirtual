8d29e1175ea50158f6eae4a6ceadcc6625926d1a
Bruno Mota <bruengprod@gmail.com>
2021-11-21 07:18:56 -0300

    Adicionado comentário em propriedade do fonte Startup.cs

--- Seção 02/Primeiro Projeto/ProjetoZero/ProjetoZero/Startup.cs
+++ Seção 02/Primeiro Projeto/ProjetoZero/ProjetoZero/Startup.cs
@@ -18,7 +18,8 @@ namespace ProjetoZero
         {
             Configuration = configuration;
         }
-
+	
+        //Propriedade de Configuração
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
